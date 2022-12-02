f584fad28c295f0df052e9902e729f661fd66f56
weewoo123 <samuelhe2014@gmail.com>
2022-12-02 02:49:27 -0600

    added everything but project info page

--- src/App.js
+++ src/App.js
@@ -2,20 +2,52 @@ import FileUpload from './components/FileUpload';
 import Description from './components/Description'
 import 'bootstrap/dist/css/bootstrap.css';
 import React from 'react';
-import Container from 'react-bootstrap/esm/Container';
-import Navbar from 'react-bootstrap/Navbar';
+import Navbar from './components/NavBar';
+import Button from 'react-bootstrap/Button'
+import { useRef } from 'react';
+import './App.css'
 
 function App() {
+  const ref = useRef(null);
+
+  const handleClick = () => {
+      ref.current?.scrollIntoView({behavior: 'smooth', block: 'center'});
+  }
+
   return (
-    <div className="App">
-      <Navbar expand = "lg" bg = "success">
-        <Container fluid>
-          <Navbar.Brand className = "text-light"> Soybean Predictor </Navbar.Brand>
-        </Container>
-      </Navbar>
-      <Description/>
-      <FileUpload/>
-    </div>
+    <>
+      <style type = "text/css">
+                  {`
+                      .btn-start {
+                          color: white;
+                          outline-style: solid;
+                      }
+                      .btn-start:hover {
+                          background-color: #CBDDD1;
+                          color: black;
+                          outline-style: solid;
+                          outline-color: #CBDDD1;
+                      }
+                  `}
+      </style>
+      <div className="App">
+        <Navbar/>
+        <div className = "home-text">
+            <div className = "home-text-2">
+              <h1>Beanify</h1>
+              <h3>A Soybean Classifier</h3>
+              <br></br>
+              <Button variant='start' onClick = {handleClick}>Get Started</Button>
+            </div>
+        </div>
+        <div className = "function" ref = {ref}>
+          <FileUpload/>
+        </div>
+        <div className = "description">
+          <Description/>
+        </div>
+      </div>
+    </>
   );
 }
 
--- src/components/FileUpload.js
+++ src/components/FileUpload.js
@@ -5,7 +5,8 @@ import Row from 'react-bootstrap/Row'
 import Col from 'react-bootstrap/Col'
 import Button from 'react-bootstrap/Button'
 import 'bootstrap/dist/css/bootstrap.css';
-
+import Results from './Results'
+import '../App.css'
 
 class FileUpload extends Component {
 
@@ -22,24 +23,28 @@ class FileUpload extends Component {
         showAlert: false
         };
       }
+    
 
     handlePredictClick = (event) => {
-        const file = this.state.file;
-        console.log(file)
-        var formdata = new FormData();
-        formdata.append("image", file);
-        this.setState({isLoading: true});
+        if(this.state.file){
+            const file = this.state.file;
+            var formdata = new FormData();
+            formdata.append("image", file);
+            this.setState({isLoading: true});
 
-        var requestOptions = {
-            method: 'POST',
-            body: formdata,
-            redirect: 'follow'
-        };
+            var requestOptions = {
+                method: 'POST',
+                body: formdata,
+                redirect: 'follow'
+            };
 
-        fetch("https://soy-api2.herokuapp.com/predict", requestOptions)
-        .then(response => response.text())
-        .then(res => this.setState({isLoading: false, result: res}))
-        .catch(error => console.log('error', error));
+            fetch("https://soy-api2.herokuapp.com/predict", requestOptions)
+            .then(response => response.text())
+            .then(res => {this.setState({isLoading: false, result: JSON.parse(res)});
+                            console.log(res)
+                            })
+            .catch(error => console.log('error', error));         
+        }
     }
 
     getExtension(filename) {
@@ -66,33 +71,67 @@ class FileUpload extends Component {
     render(){
         const imageUrl = this.state.imageUrl;
         return(
-            <Container>
-                <div className="content">
-                    <Form>
-                        <Form.Group controlId = "formGroup">
-                            <Form.Label>Upload Image (.jpg only):</Form.Label>
-                            <Form.Control 
-                                type="file"
-                                name="image"
-                                value = {imageUrl}
-                                onChange = {this.handleChange}
-                                >
-                            </Form.Control>
-                        </Form.Group>
-                        
-                        <Row>
-                            <Col>
-                                <Button
-                                    variant = "success"
-                                    onClick = {this.handlePredictClick}>
-                                    Analyze
-                                </Button>
+            <>
+            <style type = "text/css">
+                {`
+                    .btn-analyze {
+                        color: white;
+                        outline-style: solid;
+                    }
+                    .btn-analyze:hover {
+                        background-color: #CBDDD1;
+                          color: black;
+                          outline-style: solid;
+                          outline-color: #CBDDD1;
+                    }
+                `}
+            </style>
+                <Container classname = "image-upload">
+                    <div className="content">
+                        <Row className = "prediction-container">
+                            <Col className = "col1">
+                                <Form>
+                                        <Form.Group className = "upload" controlId = "formGroup">
+                                            <Form.Label style = {{}}>Upload an image:</Form.Label>
+                                            <Form.Control 
+                                                type="file"
+                                                name="image"
+                                                value = {imageUrl}
+                                                onChange = {this.handleChange}
+                                                >
+                                            </Form.Control>
+                                        </Form.Group>
+                                        <Row>
+                                            <Col>
+                                                <Button
+                                                    variant = "analyze"
+                                                    onClick = {this.handlePredictClick}>
+                                                    Analyze
+                                                </Button>
+                                            </Col>
+                                        </Row>
+                                </Form>
+                            </Col>
+                            <Col className = "col2">
+                                {this.state.isLoading ? <div className="spinner-container">
+                                                            <div className="loading-spinner">
+                                                            </div>
+                                                        </div>:
+
+                                                        (this.state.result !== "" ?
+                                                        <div>
+                                                            <p> {this.state.result.message} </p>
+                                                            <Results accuracy = {Math.round(this.state.result.accuracy * 100,2)} />
+                                                        </div> : null)}
                             </Col>
                         </Row>
-                    </Form>
-                </div>
-                <p>{this.state.result}</p>
-            </Container>
+                        
+                    </div>
+                    <br>
+                    </br>
+                    
+                </Container>
+            </>
         );
     }
 }
